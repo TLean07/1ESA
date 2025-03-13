2988990f30526e7fa2830727558c911623363d03
Leandro <125459372+TLean07@users.noreply.github.com>
2025-03-13 11:30:52 -0300

    commit

--- Python/1ESA_13_03.py
+++ Python/1ESA_13_03.py
@@ -43,6 +43,7 @@ soma = a + b
 print(soma)
 '''
 
+'''
 num1 = float(input("Digite o primeiro número: "))
 print(num1)
 num2 = float(input("Digite o segundo número: "))
@@ -81,3 +82,30 @@ else:
     operador = "?"
 
 print(f"\nResultado: {num1} {operador} {num2} = {resultado}")
+'''
+
+'''
+a = 2
+b = 3
+print(f"{a} != {b} and {a} < {b}, ou seja, {2 != 3} and {3 > 2} dá {2 != 3 and 3 > 2}")
+print(f"{a} > {b} and {a} < {b}, ou seja, {2 < 3} and {3 <  2} dá {2 > 3 and 3 > 2}")
+print(f"{a} < {b} and {a} > {b}, ou seja, {2 > 3} and {3 > 2} dá {2 > 3 and 3 > 2}")
+print(f"{a} < {b} and {a} != {b}, ou seja, {2 > 3} and {3 < 2} dá {2 > 3 and 3 > 2}")
+print("")
+print(f"{a} != {b} or {a} < {b}, ou seja, {2 != 3} or {3 > 2} dá {2 != 3 and 3 > 2}")
+print(f"{a} > {b} or {a} < {b}, ou seja, {2 < 3} or {3 > 2} dá {2 != 3 and 3 > 2}")
+print(f"{a} < {b} or {a} > {b}, ou seja, {2 > 3} or {3 > 2} dá {2 != 3 and 3 > 2}")
+print(f"{a} < {b} or {a} != {b}, ou seja, {2 > 3} or {3 < 2} dá {2 > 3 and 3 > 2}")
+'''
+while True:
+    try:
+        idoso = int(input("Qual é sua idade? "))
+        break
+    except ValueError:
+        print("Por favor digite um número válido para idade.")
+cartao = input("Você tem o cartão de idoso? (sim/não) ")
+deficiente = input("Você é deficiente? (sim/não) ")
+if idoso >= 60 and cartao == 'Sim' or 'sim' or deficiente == 'Sim' or 'sim':
+    print("Pode estacionar!")
+else:
+    print("Não pode estacionar")
