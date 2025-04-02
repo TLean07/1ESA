e5deb5e74a9be5839576c176da4aa13adbd02e5d
Leandro <125459372+TLean07@users.noreply.github.com>
2025-04-02 09:24:25 -0300

    Update script.js

--- Web/aula04/script.js
+++ Web/aula04/script.js
@@ -1,8 +1,2 @@
 console.log("Hello JavaScript!");
 document.querySelector("h1").style.color = "blue";
-document.addEventListener("DOMContentLoaded", () => {
-    const message = document.createElement("h1");
-    message.textContent = "Vai se fuder Febba";
-message.style.color = "red";
-    document.body.appendChild(message);
-});
